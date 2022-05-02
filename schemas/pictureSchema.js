819ededed0c39e59a7dd35edb9ad78fcfd5c5403
TuomasB73 <tuomasbergholm@gmail.com>
2022-05-02 19:30:37 +0300

    Get all pictures added

--- schemas/pictureSchema.js
+++ schemas/pictureSchema.js
@@ -2,6 +2,7 @@ import { gql } from 'apollo-server-express';
 
 export default gql`
   extend type Query {
+    getAllPictures: [Picture]
     getAllPicturesByCarId(car: ID!): [Picture]
     getPictureById(id: ID!): Picture
   }
